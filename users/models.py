2f8f31577e9795edfaceb0dcaad7854e40190cc7
Alina Baranova <alina_nemo@mail.ru>
2024-11-24 16:08:08 +0300

    Add fields for model User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(models.Model):
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    phone = PhoneNumberField(
+        region="RU", **NULLABLE, verbose_name="Телефон", help_text="Укажите телефон"
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatar/",
+        **NULLABLE,
+        verbose_name="Аватар",
+        help_text="Загрузите аватар"
+    )
+
+
+USERNAME_FIELD = "email"
+REQUIRED_FIELDS = []
+
+
+def __str__(self):
+    return self.email
+
+
+class Meta:
+    verbose_name = "Пользователь"
+    verbose_name_plural = "Пользователи"
